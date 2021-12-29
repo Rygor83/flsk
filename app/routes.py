3de4351e2ff717f0045cca26287130ab65764a3d
Rygor <pisemco@gmail.com>
2021-12-29 10:22:26 +0300

    0.3.0

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from app import app
 
-@app.route('/') 
-@app.route('/index') 
+
+@app.route('/')
+@app.route('/index')
 def index():
-	user = {'username': 'Rygor'} 
-	return render_template('index.html', title='Home', user=user) 
+    user = {'username': 'Rygor'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Minsk!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avenger movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
--- microblog.py
+++ microblog.py
@@ -2,4 +2,4 @@ from app import app
 
 # do not use on pruduction
 if __name__ == '__main__':
-	app.run()
+    app.run()
