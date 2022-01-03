7f58887b4fafe81e9fdd53062a7df4045b4acf19
Rygor <pisemco@gmail.com>
2022-01-03 19:19:04 +0300

    0.8.0

--- config.py
+++ config.py
@@ -11,3 +11,10 @@ class Config(object):
     ## pythonanywhere.com
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<pws>@Rygor.mysql.pythonanywhere-services.com/Rygor$app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['pisemco@gmail.com']
